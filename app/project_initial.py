596511f47830d4cd704a43cf30986f7ae8501c2c
Jayden <tjdrms2023@gmail.com>
2023-03-20 17:28:50 -0500

    Convert ctk to tk

--- app/project_initial.py
+++ app/project_initial.py
@@ -5,12 +5,12 @@ from glob import glob
 import os
 
 # Selecting GUI theme - dark, light , system (for system default)
-ctk.set_appearance_mode("dark")
+#ctk.set_appearance_mode("dark")
 
 # Selecting color theme - blue, green, dark-blue
-ctk.set_default_color_theme("blue")
+#ctk.set_default_color_theme("blue")
 
-app = ctk.CTk()
+app = tk.Tk()
 width, height = 800, 600
 width_screen, height_screen = app.winfo_screenwidth(), app.winfo_screenheight()
 x, y = (width_screen/2) - (width/2), (height_screen/2) - (height/2)
@@ -19,31 +19,30 @@ app.title("Farebox Configuration Assistant")
 app.iconphoto(False, ImageTk.PhotoImage(file='spx.png'))
 #app.resizable(False, False)
 
-title = ctk.CTkLabel(master=app, text="Configuration Assistant", font=("Arial", 40))
+title = tk.Label(master=app, text="Configuration Assistant", font=("Arial", 40))
 title.place(x=50, y=100)
 
 
-start = ctk.CTkFrame(master=app, width=700, height=150, fg_color="transparent")
+start = tk.Frame(master=app, width=700, height=150)
 start.place(x=50, y=210)
 
-label_start = ctk.CTkLabel(master=start,
+label_start = tk.Label(master=start,
                            text="Start",
-                           fg_color="transparent",
                            font=("Arial", 20))
 label_start.pack(side='left')
 label_start.place(x=0, y=0)
 
-button_new_project = ctk.CTkButton(master=start, text="New Project", font=("Arial", 15), fg_color="transparent", height=-1, anchor='w', hover=False)
-button_load_project = ctk.CTkButton(master=start, text="Load Project", font=("Arial", 15), fg_color="transparent", height=-1, anchor='w', hover=False)
+button_new_project = tk.Button(master=start, text="New Project", font=("Arial", 15), height=-1, anchor='w')
+button_load_project = tk.Button(master=start, text="Load Project", font=("Arial", 15), height=-1, anchor='w')
 
 button_new_project.place(x=0, y=30)
 button_load_project.place(x=0, y=55)
 
 
-recent = ctk.CTkFrame(master=app, width=700, height=150, fg_color="transparent")
+recent = tk.Frame(master=app, width=700, height=150)
 recent.place(x=50, y=370)
 
-label_recent = ctk.CTkLabel(master=recent, text="Recent", fg_color="transparent", font=("Arial", 20))
+label_recent = tk.Label(master=recent, text="Recent", font=("Arial", 20))
 label_recent.place(x=0, y=0)
 
 flist = glob('*.py')
