ae3cdda9b85c71549706f25fc00c8a2d16e51457
Mark Bauer <markb5@illinois.edu>
2023-03-20 18:09:31 -0500

    Add files via upload
    updated initial screen

--- app/project_initial.py
+++ app/project_initial.py
@@ -29,30 +29,58 @@ start.place(x=50, y=210)
 label_start = tk.Label(master=start,
                            text="Start",
                            font=("Arial", 20))
-label_start.pack(side='left')
-label_start.place(x=0, y=0)
+label_start.pack(padx=0, pady=5)
+# label_start.place(x=0, y=0)
 
 button_new_project = tk.Button(master=start, text="New Project", font=("Arial", 15), height=-1, anchor='w')
 button_load_project = tk.Button(master=start, text="Load Project", font=("Arial", 15), height=-1, anchor='w')
 
-button_new_project.place(x=0, y=30)
-button_load_project.place(x=0, y=55)
+button_new_project.pack(padx=5, pady=5)
+button_load_project.pack(padx=5, pady=5)
 
 
-recent = tk.Frame(master=app, width=700, height=150)
-recent.place(x=50, y=370)
+# recent = tk.Frame(master=app, width=700, height=150)
+# recent.pack(padx=100, pady=200)
 
-label_recent = tk.Label(master=recent, text="Recent", font=("Arial", 20))
-label_recent.place(x=0, y=0)
+label_recent = tk.Label(master=start, text="Recent", font=("Arial", 20))
+label_recent.pack(padx=5, pady=5)
 
 flist = glob('*.py')
 flist.sort(key=os.path.getmtime, reverse=True)
 
-listbox_recent = tk.Listbox(master=recent)
-listbox_recent.place(x=0, y=25)
+listbox_recent = tk.Listbox(master=start)
+listbox_recent.pack(padx=5, pady=5)
 
 num_to_show = min(len(flist), 5)
 for i in range(num_to_show):
     listbox_recent.insert(tk.END, flist[i])
+
+menubar = tk.Menu(start, bd=3, relief=tk.RAISED, activebackground="#80B9DC")
+
+# Filemenu
+filemenu = tk.Menu(master=menubar, tearoff=0, relief=tk.RAISED, activebackground="#026AA9")
+menubar.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="New Project")
+filemenu.add_command(label="Close")
+filemenu.add_separator()
+filemenu.add_command(label="Exit")  
+
+# proccessing menu
+processing_menu = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Validation", menu=processing_menu)
+processing_menu.add_command(label="validate")
+processing_menu.add_separator()
+
+# help menu
+help_menu = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="About",)
+help_menu.add_separator()
+app.config(menu=menubar)
+
+
+
+
+
 
 app.mainloop()  
